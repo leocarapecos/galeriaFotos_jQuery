535f3d32f8563748d478e81cc34724af7f45181c
Leonardo Carapecos <leonardocarapecosdev@gmail.com>
2023-06-14 21:18:09 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('header button').click(function(){
+        $('form').slideDown();
+    })
+
+    $('#btn-cancelar').click(function(){
+        $('form').slideUp();
+    })
+
+    $('form').on('submit',function(e){
+        e.preventDefault();
+        const novaImagem = $('#endereco-img-nova').val();
+        const novoItem = $('<li style="display:none"></li>')
+        $(`<img src="${novaImagem}"/>`).appendTo(novoItem);
+        $(`
+            <div class="img-link">
+                	<a href="${novaImagem}" target="_blank" title="Ver imagem em tamanho real">
+                        Ver imagem em tamanho real
+                    </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul')
+        $(novoItem).fadeIn(1000);
+        $('#endereco-img-nova').val('')
+
+    })
+
+
+})  
+
